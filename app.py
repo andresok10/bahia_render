967d05e654eb96bbadb26cf65e70eec11711f1f4
andresok10 <davexgarcia@gmail.com>
2026-01-31 11:49:58 -0500

    guardarok

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ db.init_app(app)
 csrf = CSRFProtect(app)
 
 with app.app_context():
-    if Cat.query.count() == 0:
-        init_db()
-    else:
-        print("Base de datos ya inicializada")
+    #if Cat.query.count() == 0:
+    init_db()
+    #else:
+    #    print("Base de datos ya inicializada")
     #init_db()
     total = Articulos.query.count()
     print("Registros en la BD:", total)
